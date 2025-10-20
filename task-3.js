36eeab864de16d9dbadf777f9ff6c70d9a78404e
Admin <lubomyrfalfushinskyi@gmail.com>
2025-10-20 23:45:31 +0300

    ver1

---
+++ task-3.js
@@ -0,0 +1,22 @@
+const profile = {
+  username: "Jacob",
+  playTime: 300,
+  getInfo(){
+    return `${this.username} has ${this.playTime} hours!`;
+  },
+  changeUsername(name){
+    this.username = name;
+  },
+  updatePlayTime(time){
+    this.playTime += time;
+  },
+
+};
+
+console.log(profile.getInfo()); // "Jacob has 300 active hours!"
+
+profile.changeUsername("Marco");
+console.log(profile.getInfo()); // "Marco has 300 active hours!"
+
+profile.updatePlayTime(20);
+console.log(profile.getInfo()); // "Marco has 320 active hours!"
